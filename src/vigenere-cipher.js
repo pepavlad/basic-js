0b0f3cd5e8351ee9f53ddebd1fbbeb4fe936304e
pepavlad <pepelvlad@gmail.com>
2020-03-21 15:10:45 +0300

    feat: add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,53 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(rev = true){
+        this.rev = rev;
+        this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(str, key){ 
+        return this.execute(str, key, 'encrypt')
+    }
+
+    decrypt(str, key) {
+        return this.execute(str, key, 'decrypt')
+    }
+
+    execute(str, key, operation) {
+        if(!str || !key) throw new Error();
+        let result = [];
+        let keyIndex = 0;
+        str = str.toUpperCase();
+        key = key.toUpperCase();
+        switch (operation) {
+            case 'encrypt':
+                result = str.toUpperCase().split('').map(item => {
+                    if (item >= 'A' && item <= 'Z'){
+                        let strIndex = (this.alphabet.indexOf(key[keyIndex % key.length])+ this.alphabet.indexOf(item)) % 26;
+                        let letter = this.alphabet[strIndex];
+                        keyIndex++;
+                        return letter;
+                    }
+                    return item;
+                });
+                break;
+            case 'decrypt':
+                result = str.toUpperCase().split('').map(item => {
+                    if (item >= 'A' && item <= 'Z'){    
+                        let strIndex = this.alphabet.indexOf(item) - this.alphabet.indexOf(key[keyIndex % key.length]);     
+                        if(strIndex < 0){
+                            strIndex +=  26;
+                        }
+                        let letter = this.alphabet[strIndex];
+                        keyIndex++;          
+                        return letter;
+                    }
+                    return item;
+                });
+                break;
+        }
+        if(this.rev){
+            return result.join('');
+        } else return result.reverse().join('');
     }
 }
 
